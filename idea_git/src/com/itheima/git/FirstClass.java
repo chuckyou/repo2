fe395af1fbe337c9ca4037078fc67b1ea2d822f3
admin <zhangsan@itcast.cn>
2018-10-16 20:53:04 +0800

    在FirstClass中添加了test方法

--- idea_git/src/com/itheima/git/FirstClass.java
+++ idea_git/src/com/itheima/git/FirstClass.java
@@ -8,9 +8,14 @@ public class FirstClass {
         System.out.println("hello..git");
         System.out.println("hello..git");
         System.out.println("hello..git");
+        System.out.println("a little modify");
     }
 
     public void test(){
         System.out.println(".................");
+    }
+
+    public void addTest(){
+        System.out.println("see you again..git");
     }
 }
